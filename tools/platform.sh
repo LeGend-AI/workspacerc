fad644620363467732837007e2cad69c3e9fe66e
LeGend-AI <legend-ai@outlook.com>
2017-05-28 15:51:07 +0800

    password failed

--- tools/platform.sh
+++ tools/platform.sh
@@ -16,11 +16,19 @@ LG_apple() {
 
 LG_redhat() {
   echo ${passwd}'\n' | sudo -S yum makecache
+  if [ $? -ne 0 ]; then
+    echo "Update failed! Maybe password is wrong"
+    exit 1
+  fi
   export PKMGR='yum'
 }
 
 LG_debian() {
   echo ${passwd}'\n' | sudo -S apt-get update
+  if [ $? -ne 0 ]; then
+    echo "Update failed! Maybe password is wrong"
+    exit 1
+  fi
   export PKMGR='apt-get'
 }
 
@@ -34,6 +42,9 @@ LG_platform_check() {
   echo -n "Enter your password: "
   read -s passwd
   export passwd=$passwd
+  if 1 ; then
+    exit
+  fi
   if [ -f /etc/redhat-release ]; then
     LG_redhat
   elif [ -f /etc/debian_version ]; then
