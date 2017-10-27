c3fdf1cd3e04f14de1d98d0a63b0a554676f41e8
LeGend-AI <legend-ai@outlook.com>
2017-10-27 10:41:23 +0800

    refer oh-my-zsh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,6 +1,6 @@
 main() {
   if [ ! -d ~/.oh-my-zsh ]; then
-    ln -s $WORK_HOME/zsh/.oh-my-zsh $HOME
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
   fi
   if [ ! $LG_WORKSPACERC ]; then
     cat $WORK_HOME/zsh/.zshrc >> $HOME/.zshrc
