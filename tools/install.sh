8fc048e0157200ab59d3734356baad39c600fd85
Liu Guo <legend-ai@outlook.com>
2017-12-21 11:50:37 +0800

    clone repo before settings

--- tools/install.sh
+++ tools/install.sh
@@ -3,7 +3,7 @@ main() {
   WORK_HOME=~/.workspacerc
 
   # require git
-  #git clone https://github.com/legend-ai/workspacerc $WORK_HOME
+  git clone https://github.com/legend-ai/workspacerc $WORK_HOME
 
   # require vim
   . $WORK_HOME/vim/setup.sh
